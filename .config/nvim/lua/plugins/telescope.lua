ac1d7dee09b8127b40927888d00a4b54f699a746
Rémy Frenoy <remy@dust.tt>
2026-06-26 09:51:20 +0200

    Add live_grep args and iglob

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -4,6 +4,7 @@ return {
     dependencies = {
         'nvim-lua/plenary.nvim',
 	'BurntSushi/ripgrep',
+	'nvim-telescope/telescope-live-grep-args.nvim',
     },
     keys = function()
         local lazy_telescope = function(builtin)
@@ -11,12 +12,15 @@ return {
                 require('telescope.builtin')[builtin](...)
             end
         end
+        local live_grep_args = function(...)
+            require('telescope').extensions.live_grep_args.live_grep_args(...)
+        end
         return {
             { '<leader>fb', lazy_telescope('buffers'),                   desc = 'Find buffers' },
             { '<leader>fd', lazy_telescope('diagnostics'),               desc = 'Find diagnostics' },
             { '<leader>fg', lazy_telescope('git_files'),                 desc = 'Find Git files' },
             { '<leader>ff', lazy_telescope('find_files'),                desc = 'Find files' },
-            { '<leader>/', lazy_telescope('live_grep'),                 desc = 'Find files by content' },
+            { '<leader>/', live_grep_args,                              desc = 'Find files by content' },
             { '<leader>fh', lazy_telescope('help_tags'),                 desc = 'Find help tags' },
             { '<leader>fo', lazy_telescope('oldfiles'),                  desc = 'Find recently opened files' },
             { '<leader>fw', lazy_telescope('grep_string'),               desc = 'Find word in buffer' },
@@ -24,8 +28,12 @@ return {
             { '<leader>fc', lazy_telescope('commands'), desc = 'Find commands' },
         }
     end,
-    config = true,
+    config = function(_, opts)
+        require('telescope').setup(opts)
+        require('telescope').load_extension('live_grep_args')
+    end,
     opts = function()
+        local lga_actions = require('telescope-live-grep-args.actions')
 	local function find_command()
             if 1 == vim.fn.executable("rg") then
               return { "rg", "--files", "--color", "never", "-g", "!.git" }
@@ -63,6 +71,31 @@ return {
                     additional_args = function()
                         return { "--hidden", "--glob=!.git/" }
                     end,
+                },
+            },
+            extensions = {
+                live_grep_args = {
+                    auto_quoting = true,
+                    -- search hidden files and skip .git, matching the live_grep picker
+                    vimgrep_arguments = {
+                        "rg",
+                        "--color=never",
+                        "--no-heading",
+                        "--with-filename",
+                        "--line-number",
+                        "--column",
+                        "--smart-case",
+                        "--hidden",
+                        "--glob=!.git/",
+                    },
+                    mappings = {
+                        i = {
+                            -- wrap the term in quotes so you can append raw rg args
+                            ["<C-k>"] = lga_actions.quote_prompt(),
+                            -- jump straight to typing a folder glob
+                            ["<C-i>"] = lga_actions.quote_prompt({ postfix = " --iglob " }),
+                        },
+                    },
                 },
             },
         }
